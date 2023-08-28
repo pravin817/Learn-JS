0d2dc3fcf1d57e43d79592aa9e8b7ae89a0746f2
pravin817 <83346334+pravin817@users.noreply.github.com>
2023-08-28 09:06:10 +0000

    Data type added

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"  // treat all Js code as newer version
+
+// alert(3+3);  // we are using node.js not browser
+
+console.log(3+3)
+
+console.log("Pravin")
+
+let name = "Pravin"
+let age = 21
+
+let isLoggedIn = true
+
+// number 2 to power 53
+// bigInt 
+// string 
+// Boolean - true or false
+// null ---> Standalone values
+// undefined  ----> 
+// symbol ===> unique
+
+// object
+
+console.log(typeof(name))
+
+console.log(typeof(null))        // object
+console.log(typeof(undefined))   // undefined
