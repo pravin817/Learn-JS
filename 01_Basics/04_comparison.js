bfa40ca79fd57bc7b621034ab85d6f395f463557
pravin817 <83346334+pravin817@users.noreply.github.com>
2023-08-29 19:37:25 +0000

    Comparion Added

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,32 @@
+// console.log(3>2);
+// console.log(3>=2);
+// console.log(3<2);
+// console.log(3<=2);
+// console.log(3==2);
+// console.log(3!=2);
+
+
+// console.log("2">1);  // true
+// console.log("02">1);  // true
+
+/*
+    Note : Avoid this type of the compariosn to avoid the unpredicatability issues
+*/    
+
+// console.log(null > 0);    // false
+// console.log(null == 0);   // false
+// console.log(null >= 0);   // true
+
+
+// console.log(undefined > 0);    // false
+// console.log(undefined == 0);   // false
+// console.log(undefined >= 0);   // false
+
+// strict check  (===) checks both the data and the data type of the variable
+
+// console.log("2" == 2);     // true
+// console.log("2" === 2);    // false
+
+
+// console.log("2">"1");    // true
+// console.log(2>"1");      // true
