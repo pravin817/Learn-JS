0f48b87354f1b04c130caa4dcf597258c93b30cd
pravin817 <83346334+pravin817@users.noreply.github.com>
2023-09-18 08:16:03 +0000

    The object basic 2 added successfully.

---
+++ 02_Basics/04.objects.js
@@ -0,0 +1,86 @@
+// const tinderUser = new Object();   // This is a singleton object as we created it with the help of the constructor
+
+// const tinderUser2 = {};
+
+// console.log(tinderUser);
+// console.log(tinderUser2);
+
+const tinderUser = {};
+
+tinderUser.id = "123abc";
+tinderUser.name = "sammy";
+tinderUser.isLoggedIn = false;
+
+// console.log(tinderUser);
+
+const regularUser = {
+  email: "some@gmail.com",
+  fullName: {
+    userFullName: {
+      firstName: "Pravin",
+      lastName: "Mhaske",
+    },
+  },
+};
+
+// ?. This is know as the Optional chaining ---> We don't want to use nested if...else
+
+console.log(regularUser.fullName?.userFullName.firstName);
+
+// We can combine the object
+
+const obj1 = {
+  1: "a",
+  2: "b",
+};
+
+const obj2 = {
+  3: "c",
+  4: "d",
+};
+
+const obj4 = { 5: "a", 6: "b" };
+
+// const obj3 = {obj1,obj2};
+
+// const obj3 = Object.assign({},obj1,obj2,obj4);
+
+const obj3 = { ...obj1, ...obj2, ...obj4 };
+
+console.log(obj3);
+
+// If we get the data from the DB
+
+const user = [
+  {
+    id: 1,
+    email: "p1@gmail.com",
+  },
+  {
+    id: 2,
+    email: "p2@gmail.com",
+  },
+  {
+    id: 3,
+    email: "p3@gmail.com",
+  },
+];
+
+// user.map((u)=>{
+//     console.log(u);
+// })
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));  // The return type is the array
+
+console.log(Object.values(tinderUser));
+
+console.log(Object.entries(tinderUser));
+
+// To check the property is belong to that object
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+
+
